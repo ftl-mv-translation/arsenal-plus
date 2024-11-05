3084b4d9ce8796bf3c954c1641364879a2edb2bd
ranhai613 <85445781+ranhai613@users.noreply.github.com>
2024-11-06 01:24:23 +0900

    prepare for mass edits

---
+++ snippets/mass_edits_en_vanilla_texts.py
@@ -0,0 +1,6 @@
+from json import load
+from snippets.mass_edits import mass_translate
+
+with open(f'snippets/vanilla-texts/text-ru.json', encoding='utf8') as f:
+    text_map = load(f)
+mass_translate(f'locale/**/en.po', {v: k for k, v in text_map.items()}, overwrite=False)
